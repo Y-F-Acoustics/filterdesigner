5beec7b1abbe20f6a53c29a95fef7ec5e0158faf
Yuki Fukuda <34057035+Yuki-F-HCU@users.noreply.github.com>
2020-03-09 01:18:59 +0900

    Add IIRDesign module (making now.)

---
+++ IIRDesign/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 01:12:05 2020
+
+@author: Yuki Fukuda
+"""
+
+from .functions import *
---
+++ IIRDesign/functions.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 01:00:17 2020
+
+@author: Yuki Fukuda
+"""
+
+import scipy.signal as signal
+
