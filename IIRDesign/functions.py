d1e06f18762b405119429a021ae55655a7767661
Yuki Fukuda <34057035+Yuki-F-HCU@users.noreply.github.com>
2020-03-16 01:28:10 +0900

    Bug fixes and add buttord function.

--- IIRDesign/functions.py
+++ IIRDesign/functions.py
@@ -7,8 +7,9 @@ Created on Mon Mar  9 01:00:17 2020
 
 import scipy.signal as signal
 from typing import List, Tuple
+import numpy as np
 
-def butter(n : int, Wn, ftype :str='default', ds :str= 'd') -> Tuple[float, float]:
+def butter(n : int, Wn, ftype :str='default', zs :str= 'z') -> Tuple:
     """
     Butterworth digital and analog filter design.
 
@@ -35,9 +36,9 @@ def butter(n : int, Wn, ftype :str='default', ds :str= 'd') -> Tuple[float, floa
     ftype : {'default', 'lowpass', 'highpass', 'bandpass', 'bandstop'}, optional
         The type of filter. The default is 'default'.
         
-    ds : {'d', 's'}, optional
+    zs : {'z', 's'}, optional
         When 's', return an analog filter, otherwise a digital filter is returned. 
-        The default is 'd'.
+        The default is 'z'.
 
     Returns
     -------
@@ -50,7 +51,7 @@ def butter(n : int, Wn, ftype :str='default', ds :str= 'd') -> Tuple[float, floa
     """
     
     ftypelist = ['low', 'high', 'bandpass', 'stop', 'default']
-    dslist = ['d', 's']
+    zslist = ['z', 's']
     analog = False
     fs = None
     
@@ -58,48 +59,48 @@ def butter(n : int, Wn, ftype :str='default', ds :str= 'd') -> Tuple[float, floa
         raise ValueError("`ftype` must be 'low', 'high', 'bandpass', 'stop',"
                          + " or 'default'.")
         
-    if (ds in dslist) == False:
-        raise ValueError("`ds` must be 'd' or 's'.")
+    if (zs in zslist) == False:
+        raise ValueError("`zs` must be 'z' or 's'.")
         
-    if ds == 'd':
-        if type(Wn) == list:
-            if max(Wn) >= 1.0 or min(Wn) < 0.0:
-                raise ValueError("When `ds` is 'd', value of `Wn` must be from"
+    if zs == 'z':
+        if type(Wn) in [list, np.ndarray]:
+            if np.max(Wn) >= 1.0 or np.min(Wn) < 0.0:
+                raise ValueError("When `zs` is 'z', value of `Wn` must be from"
                                  + "0 to 1.")
         else:
             if Wn >= 1.0 or Wn < 0.0:
-                raise ValueError("When `ds` is 'd', value of `Wn` must be from"
+                raise ValueError("When `zs` is 'z', value of `Wn` must be from"
                                  + "0 to 1.")
             
         
     if ftype == 'default':
-        if type(Wn) == float:
+        if type(Wn) in [float, np.float, np.float16, np.float32, np.float64]:
             ftype = 'lowpass'
         else:
             ftype = 'bandpass'
     elif ftype == 'low':
-        if type(Wn) == list:
+        if type(Wn) in [list, np.ndarray]:
             raise ValueError("`Wn` must be float when `ftype` is 'low'.")
         else:
             ftype = 'lowpass'
     elif ftype == 'high':
-        if type(Wn) == list:
+        if type(Wn) in [list, np.ndarray]:
             raise ValueError("`Wn` must be float when `ftype` is 'high'.")
         else:
             ftype = 'highpass'
     elif ftype == 'stop':
-        if type(Wn) == float:
+        if type(Wn) in [float, np.float, np.float16, np.float32, np.float64]:
             raise ValueError("`Wn` must be sequence when `ftype` is 'stop'.")
         else:
             ftype = 'bandstop'
     else:
         #bandpass filter
-        if type(Wn) == float:
+        if type(Wn) in [float, np.float, np.float16, np.float32, np.float64]:
             raise ValueError("`Wn` must be sequence when `ftype` is 'band'.")
         else:
             ftype = 'bandpass'
             
-    if ds == 's':
+    if zs == 's':
         analog = True
     else:
         fs = 2
@@ -109,10 +110,82 @@ def butter(n : int, Wn, ftype :str='default', ds :str= 'd') -> Tuple[float, floa
     return num, den
 
 
+def buttord(Wp, Ws, Rp, Rs, zs:str='z')->Tuple:
+    """
+    Butterworth filter order selection.
+    
+    Return the order of the lowest order digital or analog Butterworth filter 
+    that loses no more than Rp dB in the passband and has at least Rs dB 
+    attenuation in the stopband.
+    
+    Parameters
+    ----------
+    Wp, Ws : float
+        Passband and stopband edge frequencies.
+        For digital filters, these are in the same units as fs. By default, 
+        fs is 2 half-cycles/sample, so these are normalized from 0 to 1, 
+        where 1 is the Nyquist frequency. 
+        (wp and ws are thus in half-cycles / sample.) 
+        
+        For example:
+            ・ Lowpass: wp = 0.2, ws = 0.3
+            ・ Highpass: wp = 0.3, ws = 0.2
+            ・ Bandpass: wp = [0.2, 0.5], ws = [0.1, 0.6]
+            ・ Bandstop: wp = [0.1, 0.6], ws = [0.2, 0.5]
+            
+        For analog filters, wp and ws are angular frequencies (e.g. rad/s).
+        
+    Rp : float
+        The maximum loss in the passband (dB).
+        
+    Rs : float
+        The minimum attenuation in the stopband (dB).
+        
+    zs : {'z', 's'}, optional
+        When 's', return an analog filter, otherwise a digital filter is 
+        returned.
+        The default is 's'.
+        
+    Returns
+    -------
+    n : int
+        The lowest order for a Butterworth filter which meets specs.
+        
+    Wn : ndarray or float
+        The Butterworth natural frequency (i.e. the “3dB frequency”). 
+        Should be used with butter to give filter results. 
+    """
+    
+    zslist = ['z', 's']
+    
+    # Default parameters.
+    analog = False
+    fs = None
+    
+    # Digital or analog
+    if (zs in zslist) == False:
+        raise ValueError("`zs` must be 'z' or 's'.")
+    
+    # When analog filter
+    if zs == 's':
+        analog = True
+    else:
+        fs = 2
+      
+    # Calcurate filter order and cutoff frequencies using signal.buttord
+    n, Wn = signal.buttord(Wp, Ws, Rp, Rs, analog=analog, fs=fs)
+    
+    # Return results.
+    return int(n), Wn
+    
+    
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import numpy as np
     
-    num, den = butter(21, 0.6)
+    n, Wn = buttord(0.6, 0.7, 1, 120)
+    num, den = butter(n, Wn)
     x = signal.freqz(num, den, worN = None, fs = 2.0)
     plt.plot(x[0], np.abs(x[1]))
