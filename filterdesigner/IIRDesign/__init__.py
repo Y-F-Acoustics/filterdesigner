2c3a5a5fc614ef5ab895190c519eb1f0a95da179
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-10-20 23:19:27 +0900

    Update __init__.py
    Separate each functions

--- filterdesigner/IIRDesign/__init__.py
+++ filterdesigner/IIRDesign/__init__.py
@@ -5,4 +5,15 @@ Created on Mon Mar  9 01:12:05 2020
 @author: Yuki Fukuda
 """
 
-from .functions import *
+from ._butter import butter
+from ._buttord import buttord
+from ._cheby1 import cheby1
+from ._cheby1ord import cheb1ord
+from ._cheby2 import cheby2
+from ._cheby2ord import _cheby2ord
+from ._ellip import ellip
+from ._ellipord import ellipord
+from ._iirnotch import iirnotch
+from ._iirpeak import iirpeak
+from ._polyscale import polyscale
+from ._polystab import polystab
