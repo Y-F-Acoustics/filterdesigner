1e4a2069b9a4c86fa76ffaf82f2438ffa64fc4da
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-10-24 16:08:53 +0900

    Upload test_fir2.py
    Test function for fir2

---
+++ filterdesigner/tests/test_fir2.py
@@ -0,0 +1,37 @@
+import unittest
+import filterdesigner.FIRDesign as FIRDesign
+import scipy.signal as signal
+import numpy as np
+
+class TestFIR2(unittest.TestCase):
+    def setUp(self):
+        self.nhi = 33
+        self.fhi = [0, 0.48, 0.48, 1]
+        self.mhi = [0, 0, 1, 1]
+        self.nlo = 30
+        self.flo = [0, 0.6, 0.6, 1]
+        self.mlo = [1, 1, 0, 0]
+        self.nA = 50
+
+    def test_fir2_1(self):
+        # Highpass filter with bad filter order.
+        self.asserTrue(FIRDesign.fir2(self.nhi, self.fhi, self.mhi) == (signal.firwin2(35, self.fhi, self.mhi), 1))
+
+    def test_fir2_2(self):
+        # Lowpass filter
+        self.assertTrue(FIRDesign.fir2(self.nlo, self.flo, self.mlo) == (signal.firwin2(31, self.flo, self.mlo), 1))
+
+    def test_fir2_3(self):
+        # Arbitrary Magnitude filter
+        F1 = np.arange(0, 0.181, 0.01)
+        A1 = 0.5 + np.sin(2*np.pi*7.5*F1)/4
+        F2 = [0.2, 0.38, 0.4, 0.55, 0.562, 0.585, 0.6, 0.78]
+        A2 = [0.5, 2.3, 1, 1, -0.2, -0.2, 1, 1]
+        F3 = np.arange(0.79, 1.01, 0.01)
+        A3 = 0.2 + 18 * (1-F3) ** 2
+
+        FA = np.hstack((F1, F2, F3))
+        AA = np.hstack((A1, A2, A3))
+
+        self.assertTrue(FIRDesign.fir2(self.nA, FA, AA) == (signal.firwin2(51, FA, AA), 1))
+        
