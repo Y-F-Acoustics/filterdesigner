8bad6880fa3feaf201a11f4dca78098654f6ce29
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-10-24 14:36:38 +0900

    Add files via upload

--- filterdesigner/FIRDesign/_fir2.py
+++ filterdesigner/FIRDesign/_fir2.py
@@ -14,8 +14,12 @@ def fir2(n : int, f, m, npt : int =512, window=None) -> Tuple:
     Parameters
     ----------
     n : int
-        The number of taps in the FIR filter.  `n` must be less than
-        `npt`.
+        Filter order, specified as an integer scalar.
+
+        For configurations with a passband at the Nyquist frequency, 
+        fir2 always uses an even order. 
+        If you specify an odd-valued n for one of those configurations, 
+        then fir2 increments n by 1.
         
     f : array_like, 1D
         The frequency sampling points. Typically 0.0 to 1.0 with 1.0 being
@@ -50,9 +54,12 @@ def fir2(n : int, f, m, npt : int =512, window=None) -> Tuple:
     
     if npt <= n/2:
         raise ValueError('`npt` must be larger than `n/2`.')
+
+    if (m[-1] == 1 and n % 2 == 1):
+        n += 1 
     
     nfreqs = npt * 2
-        
+    n += 1
     num = signal.firwin2(n, f, m, nfreqs=nfreqs, window=window)
     den = 1
     
