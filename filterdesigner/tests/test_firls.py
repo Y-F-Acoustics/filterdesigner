f12c0af2630b77a1373283033c381efca7defb62
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-10-31 16:05:25 +0900

    Update test_firls.py
    Add testcase for odd order

--- filterdesigner/tests/test_firls.py
+++ filterdesigner/tests/test_firls.py
@@ -8,7 +8,13 @@ class TestFIRls(unittest.TestCase):
         self.n = 100
         self.f = [0, 0.15, 0.85, 1]
         self.a = [1, 1, 0, 0]
+        
+        self.n2 = 101
 
-    def test_firls(self):
+    def test_firls_1(self):
         # Test for least square method
         self.assertTrue(FIRDesign.firls(self.n, self.f, self.a) == (signal.firls(101, f, a), 1))
+        
+    def test_firls_2(self):
+        # Test for least square method with odd order
+        self.assertTrue(FIRDesign.firls(self.n2, self.f, self.a) == (signal.firls(103, f, a), 1))
