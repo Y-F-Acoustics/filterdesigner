5b972aadddd96dea0ecd275ba7e3bfb5464dc735
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-02 23:17:24 +0900

    Update some files
    Fix typo

--- filterdesigner/IIRDesign/__init__.py
+++ filterdesigner/IIRDesign/__init__.py
@@ -8,9 +8,9 @@ Created on Mon Mar  9 01:12:05 2020
 from ._butter import butter
 from ._buttord import buttord
 from ._cheby1 import cheby1
-from ._cheby1ord import cheb1ord
+from ._cheb1ord import cheb1ord
 from ._cheby2 import cheby2
-from ._cheby2ord import cheb2ord
+from ._cheb2ord import cheb2ord
 from ._ellip import ellip
 from ._ellipord import ellipord
 from ._iirnotch import iirnotch
