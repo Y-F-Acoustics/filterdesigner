d4075d5b0dface3fcd20761b2aea4f6abee699f1
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-02 23:15:20 +0900

    Upload test_buttord.py
    Initial commit

---
+++ filterdesigner/tests/test_buttord.py
@@ -0,0 +1,33 @@
+import unittest
+import filterdesigner.IIRDesign as IIRDesign
+import scipy.signal as signal
+
+class TestButtord(unittest.TestCase):
+
+    def setUp(self):
+        self.f1 = 0.2
+        self.f2 = 0.3
+        self.f3 = [0.2, 0.5]
+        self.f4 = [0.1, 0.6]
+        self.Rp = 1
+        self.Rs = 1
+
+    def test_buttord_1(self):
+        # Test case for lowpass filter
+        self.assertTrue(IIRDesign.buttord(self.f1, self.f2, self.Rp, self.Rs) == signal.buttord(self.f1, self.f2, self.Rp, self.Rs, analog=False, fs=2))
+
+    def test_buttord_2(self):
+        # Test case for highpass filter
+        self.assertTrue(IIRDesign.buttord(self.f2, self.f1, self.Rp, self.Rs) == signal.buttord(self.f2, self.f1, self.Rp, self.Rs, analog=False, fs=2))
+
+    def test_buttord_3(self):
+        # Test case for bandpass filter
+        self.assertTrue(IIRDesign.buttord(self.f2, self.f3, self.Rp, self.Rs) == signal.buttord(self.f2, self.f3, self.Rp, self.Rs, analog=False, fs=2))
+
+    def test_buttord_4(self):
+        # Test case for bandstop filter
+        self.assertTrue(IIRDesign.buttord(self.f3, self.f2, self.Rp, self.Rs) == signal.buttord(self.f3, self.f2, self.Rp, self.Rs, analog=False, fs=2))
+
+    def test_buttord_5(self):
+        # Test case for analog filter
+        self.assertTrue(IIRDesign.buttord(60, 75, self.Rp, self.Rs, zs='s') == signal.buttord(60, 75, self.Rp, self.Rs, analog=True, fs=None))
