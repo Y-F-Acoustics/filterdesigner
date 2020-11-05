2decfffdd1022856f1d9e654f6407a3817a27280
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-06 00:31:33 +0900

    Add test_iirpeak.py
    Initial commit

---
+++ filterdesigner/tests/test_iirpeak.py
@@ -0,0 +1,13 @@
+import unittest
+import filterdesigner.IIRDesign as IIRDesign
+import scipy.signal as signal
+
+class TestIirpeak(unittest.TestCase):
+
+    def setUp(self):
+        self.w0 = 0.4
+        self.bw = 0.4/35
+
+    def test_iirpeak(self):
+        # Test case
+        self.assertTrue(IIRDesign.iirpeak(self.w0, self.bw) = signal.iirpeak(self.w0, self.w0/self.bw, fs=2))
