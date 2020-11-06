b8aa44ffe7de56220a2b9d2d67e7a9baef9babde
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-07 00:14:45 +0900

    Update test_grpdelay.py

--- filterdesigner/tests/test_grpdelay.py
+++ filterdesigner/tests/test_grpdelay.py
@@ -10,7 +10,6 @@ class TestGrpdelay(unittest.TestCase):
     def setUp(self):
         self.order = 80
         self.cut = 0.5
-
         self.fc = 300
         self.fs = 1000
 
@@ -19,9 +18,12 @@ class TestGrpdelay(unittest.TestCase):
         fil = FIRDesign.fir1(self.order, self.cut)
         w, gd = signal.group_delay(fil, w=512, fs=2*np.pi)
         gd = np.round(gd)
-        self.assertTrue(FilterSpec.grpdelay(fil) == (w, gd))
+        ww, gdgd = FilterSpec.grpdelay(fil)
+        self.assertTrue(np.all(w == ww) and np.all(gd == gdgd))
     
     def test_grpdelay_2(self):
         # Test case for IIR filter
         fil = IIRDesign.butter(6, self.fc/(self.fs/2))
-        self.assertTrue(FilterSpec.grpdelay(fil) == signal.group_delay(fil, w=512, fs=2*np.pi))
+        w, gd = FilterSpec.grpdelay(fil)
+        ww, gdgd = signal.group_delay(fil, w=512, fs=2*np.pi)
+        self.assertTrue(np.all(w == ww) and np.all(gd == gdgd))
