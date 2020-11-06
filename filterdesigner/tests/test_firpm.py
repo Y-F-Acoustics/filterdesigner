48fe53b7cbc43b81f7bd4613afb5b64e2fdd9bd0
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-06 23:28:36 +0900

    Update test_firpm.py

--- filterdesigner/tests/test_firpm.py
+++ filterdesigner/tests/test_firpm.py
@@ -30,6 +30,9 @@ class TestFIRpm(unittest.TestCase):
         f2_2 = self.f2/(self.fs/2)
         f2_2[0] = 0
         f2_2[-1] = 1
+        x_w = [i for i in range(len(w))]
+        ipw = ip.interp1d(x_w, w)
+        w_new = ipw(np.linspace(x_w[0], x_w[-1], 2*len(x_w)))
         FIR = FIRDesign.firpm(self.n2, f2_2, self.a2, self.w)
-        fir = signal.remez(self.n2+1, f2_new, self.a2, self.w, fs=2)
+        fir = signal.remez(self.n2+1, f2_new, self.a2, w_new, fs=2)
         self.assertTrue(np.all(FIR[0] == fir))
