cffb3f95afef005545b60f27288308f3685ffda6
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-06 22:30:49 +0900

    Update test_fir2.py

--- filterdesigner/tests/test_fir2.py
+++ filterdesigner/tests/test_fir2.py
@@ -16,13 +16,13 @@ class TestFIR2(unittest.TestCase):
     def test_fir2_1(self):
         # Highpass filter with bad filter order.
         FIR = FIRDesign.fir2(self.nhi, self.fhi, self.mhi)
-        fir = signal.firwin2(35, self.fhi, self.mhi)
+        fir = signal.firwin2(35, self.fhi, self.mhi, nfreqs=1024)
         self.assertTrue(np.all(FIR[0] == fir))
 
     def test_fir2_2(self):
         # Lowpass filter
         FIR = FIRDesign.fir2(self.nlo, self.flo, self.mlo)
-        fir = signal.firwin2(31, self.flo, self.mlo)
+        fir = signal.firwin2(31, self.flo, self.mlo, nfreqs=1024)
         self.assertTrue(np.all(FIR[0] == fir))
 
     def test_fir2_3(self):
@@ -36,6 +36,6 @@ class TestFIR2(unittest.TestCase):
         FA = np.hstack((F1, F2, F3))
         AA = np.hstack((A1, A2, A3))
         FIR = FIRDesign.fir2(self.nA, FA, AA)
-        fir = signal.firwin2(51, FA, AA)
+        fir = signal.firwin2(51, FA, AA, nfreqs=1024)
         self.assertTrue(np.all(FIR[0] == fir))
         
