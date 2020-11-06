cbe2e62a1dabf7c6f673041541f7cc926768c636
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-06 11:15:36 +0900

    Update test_cheby1.py

--- filterdesigner/tests/test_cheby1.py
+++ filterdesigner/tests/test_cheby1.py
@@ -14,28 +14,43 @@ class TestCheby1(unittest.TestCase):
 
     def test_cheby1_1(self):
         # Test case for lowpass filter with default
-        self.assertTrue(IIRDesign.cheby1(self.n, self.Rp, self.Wp1) == signal.cheby1(self.n, self.Rp, self.Wp1, fs=2))
+        IIR = IIRDesign.cheby1(self.n, self.Rp, self.Wp1)
+        iir = signal.cheby1(self.n, self.Rp, self.Wp1, fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
     
     def test_cheby1_2(self):
         # Test case for lowpass filter without default
-        self.assertTrue(IIRDesign.cheby1(self.n, self.Rp, self.Wp1, ftype='low') == signal.cheby1(self.n, self.Rp, self.Wp1, fs=2))
-
+        IIR = IIRDesign.cheby1(self.n, self.Rp, self.Wp1, ftype='low')
+        iir = signal.cheby1(self.n, self.Rp, self.Wp1, fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_cheby1_3(self):
         # Test case for highpass filter
-        self.assertTrue(IIRDesign.cheby1(self.n, self.Rp, self.Wp1, ftype='high') == signal.cheby1(self.n, self.Rp, self.Wp1, btype='highpass', fs=2))
-
+        IIR = IIRDesign.cheby1(self.n, self.Rp, self.Wp1, ftype='high')
+        iir = signal.cheby1(self.n, self.Rp, self.Wp1, btype='highpass', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+       
     def test_cheby1_4(self):
         # Test case for bandpass filter with default
-        self.assertTrue(IIRDesign.cheby1(self.n, self.Rp, self.Wp2) == signal.cheby1(self.n, self.Rp, self.Wp2, fs=2))
+        IIR = IIRDesign.cheby1(self.n, self.Rp, self.Wp2)
+        iir = signal.cheby1(self.n, self.Rp, self.Wp2, fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
 
     def test_cheby1_5(self):
         # Test case for bandpass filter without default
-        self.assertTrue(IIRDesign.cheby1(self.n, self.Rp, self.Wp2, ftype='bandpass') == signal.cheby1(self.n, self.Rp, self.Wp2, fs=2))
-
+        IIR = IIRDesign.cheby1(self.n, self.Rp, self.Wp2, ftype='bandpass')
+        iir = signal.cheby1(self.n, self.Rp, self.Wp2, fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_cheby1_6(self):
         # Test case for bandstop filter
-        self.assertTrue(IIRDesign.cheby1(self.n, self.Rp, self.Wp2, ftype='stop') == signal.cheby1(self.n, self.Rp, self.Wp2, btype='bandstop', fs=2))
-
+        IIR = IIRDesign.cheby1(self.n, self.Rp, self.Wp2, ftype='stop')
+        iir = signal.cheby1(self.n, self.Rp, self.Wp2, btype='bandstop', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_cheby1_7(self):
         # Test case for analog filter
-        self.assertTrue(IIRDesign.cheby1(self.n, self.Rp, self.Wps, zs='s') == signal.cheby1(self.n, self.Rp, self.Wps, analog=True))
+        IIR = IIRDesign.cheby1(self.n, self.Rp, self.Wps, zs='s')
+        iir = signal.cheby1(self.n, self.Rp, self.Wps, analog=True)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
