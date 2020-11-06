3b6a36f1fa41ed1d068a02a97e88a9af537d1453
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-07 00:03:12 +0900

    Update test_firpm.py

--- filterdesigner/tests/test_firpm.py
+++ filterdesigner/tests/test_firpm.py
@@ -24,16 +24,15 @@ class TestFIRpm(unittest.TestCase):
         self.assertTrue(np.all(FIR[0] == fir))
 
     def test_firpm_2(self):
-        x = [i for i in range(len(self.f2))]
-        ipf = ip.interp1d(x, self.f2)
-        f2_new = ipf(np.linspace(x[0], x[-1], 2*len(x)))
         f2_2 = self.f2/(self.fs/2)
         f2_2[0] = 0
         f2_2[-1] = 1
-        f2_new2 = f2_new/(self.fs/2)
+        x = [i for i in range(len(f2_2))]
+        ipf = ip.interp1d(x, f2_2)
+        f2_new = ipf(np.linspace(x[0], x[-1], 2*len(x)))
         x_w = [i for i in range(len(self.w))]
         ipw = ip.interp1d(x_w, self.w)
         w_new = ipw(np.linspace(x_w[0], x_w[-1], 2*len(x_w)))
         FIR = FIRDesign.firpm(self.n2, f2_2, self.a2, self.w)
-        fir = signal.remez(self.n2+1, f2_new2, self.a2, w_new, fs=2)
+        fir = signal.remez(self.n2+1, f2_new, self.a2, w_new, fs=2)
         self.assertTrue(np.all(FIR[0] == fir))
