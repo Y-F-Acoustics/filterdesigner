b0468f703850ab36220d0f4a43a103736885388f
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-06 19:35:51 +0900

    Update test_butter.py

--- filterdesigner/tests/test_butter.py
+++ filterdesigner/tests/test_butter.py
@@ -16,37 +16,37 @@ class TestButter(unittest.TestCase):
         # Test case for lowpass filter with default
         IIR = IIRDesign.butter(self.n, self.fc)
         iir = signal.butter(self.n, self.fc, fs=2)
-        self.assertTrue((IIR[0] == iir[0]).all and (IIR[1] == iir[1]).all)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
     
     def test_butter_2(self):
         # Test case for lowpass filter without default
         IIR = IIRDesign.butter(self.n, self.fc, ftype='low')
         iir = signal.butter(self.n, self.fc, fs=2)
-        self.assertTrue((IIR[0] == iir[0]).all and (IIR[1] == iir[1]).all)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
         
     def test_butter_3(self):
         # Test case for highpass filter
         IIR = IIRDesign.butter(self.n, self.fc, ftype='high')
         iir = signal.butter(self.n, self.fc, btype='highpass', fs=2)
-        self.assertTrue((IIR[0] == iir[0]).all and (IIR[1] == iir[1]).all)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
        
     def test_butter_4(self):
         # Test case for bandpass filter with default
         IIR = IIRDesign.butter(self.n2, self.fc2)
         iir = signal.butter(self.n2, self.fc2, btype='bandpass', fs=2)
-        self.assertTrue((IIR[0] == iir[0]).all and (IIR[1] == iir[1]).all)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
         
     def test_butter_5(self):
         # Test case for bandpass filter without default
         IIR = IIRDesign.butter(self.n2, self.fc2, ftype='bandpass')
         iir = signal.butter(self.n2, self.fc2, btype='bandpass', fs=2)
-        self.assertTrue((IIR[0] == iir[0]).all and (IIR[1] == iir[1]).all)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
 
     def test_butter_6(self):
         # Test case for bandstop filter
         IIR = IIRDesign.butter(self.n2, self.fc2, ftype='stop')
         iir = signal.butter(self.n2, self.fc2, btype='bandstop', fs=2)
-        self.assertTrue((IIR[0] == iir[0]).all and (IIR[1] == iir[1]).all)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
 
     def test_butter_7(self):
         # Test case for analog filter
