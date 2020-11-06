3e87aff151319192e5cf94c9f4157dcddb04457b
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-07 00:41:49 +0900

    Update test_impz.py

--- filterdesigner/tests/test_impz.py
+++ filterdesigner/tests/test_impz.py
@@ -10,16 +10,14 @@ class TestImpz(unittest.TestCase):
     def setUp(self):
         self.order = 80
         self.cut = 0.5
-
         self.fc = 300
         self.fs = 1000
-
         self.n = 1000
 
     def test_impz_1(self):
         # Test case for FIR filter without n
         fil = FIRDesign.fir1(self.order, self.cut)
-        self.assertTrue(FilterSpec.impz(fil)[1] == fil[0])
+        self.assertTrue(np.all(FilterSpec.impz(fil)[1] == fil[0]))
 
     def test_impz_2(self):
         # Test case for FIR filter with n
@@ -28,29 +26,25 @@ class TestImpz(unittest.TestCase):
         x = np.zeros(len(T))
         x[0] = 1
         yout = signal.lfilter(fil[0], fil[1], x)
-
-        self.assertTrue(FilterSpec.impz(fil, n=self.n) == (T, yout))
+        tt, y = FilterSpec.impz(fil, n=self.n)
+        self.assertTrue(np.all(tt == T) and np.all(y == yout))
 
     def test_impz_3(self):
         # Test case for IIR filter without n
         fil = IIRDesign.butter(6, self.fc/(self.fs/2))
-
         dl = signal.dlti(fil[0], fil[1], dt=1/self.fs)
         i_d = signal.dimpulse(dl)
-
         T = i_d[0]
         yout = i_d[1][0]
-
-        self.assertTrue(FilterSpec.impz(fil, fs=self.fs) == (T, yout))
-
+        tt, y = FilterSpec.impz(fil, fs=self.fs)
+        self.assertTrue(np.all(tt == T) and np.all(y == yout))
+        
     def test_impz_4(self):
         # Test case for IIR filter with n
         fil = IIRDesign.butter(6, self.fc/(self.fs/2))
-
         dl = signal.dlti(fil[0], fil[1], dt=1/self.fs)
         i_d = signal.dimpulse(dl, n=self.n)
-
         T = i_d[0]
         yout = i_d[1][0]
-
-        self.assertTrue(FilterSpec.impz(fil, n=self.n, fs=self.fs) == (T, yout))
+        tt, y = FilterSpec.impz(fil, fs=self.fs)
+        self.assertTrue(np.all(tt == T) and np.all(y == yout))
