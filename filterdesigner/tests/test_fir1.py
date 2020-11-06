1cf1b1a403815af9fa00157fe33d4c4f9cd0b34c
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-06 21:13:27 +0900

    Update test_fir1.py

--- filterdesigner/tests/test_fir1.py
+++ filterdesigner/tests/test_fir1.py
@@ -12,24 +12,30 @@ class TestFIR1(unittest.TestCase):
 
     def test_fir1_1(self):
         # Test for lowpass filter with hamming window.
-        self.assertTrue(FIRDesign.fir1(self.n, self.f1) == (np.array([0.06799017, 0.86401967, 0.06799017]), 1))
+        FIR = FIRDesign.fir1(self.n, self.f1)
+        self.assertTrue(np.all(FIR[0] == np.array([0.06799017, 0.86401967, 0.06799017])))
 
     def test_fir1_2(self):
         # Test for highpass filter with hamming window.
-        self.assertTrue(FIRDesign.fir1(self.n, self.f1, ftype='high') == (np.array([-0.00859313,  0.98281375, -0.00859313]), 1))
+        FIR = FIRDesign.fir1(self.n, self.f1, ftype='high')
+        self.assertTrue(np.all(FIR[0] == np.array([-0.00859313,  0.98281375, -0.00859313])))
 
     def test_fir1_3(self):
         # Test for bandpass filter with hamming window.
-        self.assertTrue(FIRDesign.fir1(self.n, [self.f1, self.f2]) == (np.array([ 0.06301614,  0.88770441,  0.06301614]), 1))
+        FIR = FIRDesign.fir1(self.n, [self.f1, self.f2])
+        self.assertTrue(np.all(FIR[0] == np.array([ 0.06301614,  0.88770441,  0.06301614])))
 
     def test_fir1_4(self):
         # Test for bandstop filter with hamming window.
-        self.assertTrue(FIRDesign.fir1(self.n, [self.f1, self.f2], ftype='stop') == (np.array([-0.00801395,  1.0160279 , -0.00801395]), 1))
+        FIR = FIRDesign.fir1(self.n, [self.f1, self.f2], ftype='stop')
+        self.assertTrue(np.all(FIR[0] == np.array([-0.00801395,  1.0160279 , -0.00801395])))
 
     def test_fir1_5(self):
         # Test for DC-0 filter with hamming window.
-        self.assertTrue(FIRDesign.fir1(self.n, [self.f1, self.f2, self.f3, self.f4]) == (np.array([ 0.04890915,  0.91284326,  0.04890915]), 1))
+        FIR = FIRDesign.fir1(self.n, [self.f1, self.f2, self.f3, self.f4])
+        self.assertTrue(np.all(FIR[0] == np.array([ 0.04890915,  0.91284326,  0.04890915])))
 
     def test_fir1_6(self):
         # Test for DC-1 filter with hamming window.
-        self.assertTrue(FIRDesign.fir1(self.n, [self.f1, self.f2, self.f3, self.f4], ftype='DC-1') == (np.array([-0.01376344,  1.02752689, -0.01376344]), 1))
+        FIR = FIRDesign.fir1(self.n, [self.f1, self.f2, self.f3, self.f4], ftype='DC-1')
+        self.assertTrue(np.all(FIR[0] == np.array([-0.01376344,  1.02752689, -0.01376344])))
