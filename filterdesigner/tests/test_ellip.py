a292e9ec02477f40779ddee5c2bf1fb0f7123a08
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-06 17:14:02 +0900

    Update test_ellip.py

--- filterdesigner/tests/test_ellip.py
+++ filterdesigner/tests/test_ellip.py
@@ -15,28 +15,43 @@ class TestEllip(unittest.TestCase):
 
     def test_ellip_1(self):
         # Test case for lowpass filter with default
-        self.assertTrue(IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp1) == signal.ellip(self.n, self.Rp, self.Rs, self.Wp1, btype='lowpass', fs=2))
-
+        IIR = IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp1)
+        iir = signal.ellip(self.n, self.Rp, self.Rs, self.Wp1, btype='lowpass', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_ellip_2(self):
         # Test case for lowpass filter without default
-        self.assertTrue(IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp1, ftype = 'low') == signal.ellip(self.n, self.Rp, self.Rs, self.Wp1, btype='lowpass', fs=2))
-
+        IIR = IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp1, ftype = 'low')
+        iir = signal.ellip(self.n, self.Rp, self.Rs, self.Wp1, btype='lowpass', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_ellip_3(self):
         # Test case for highpass filter
-        self.assertTrue(IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp1, ftype = 'high') == signal.ellip(self.n, self.Rp, self.Rs, self.Wp1, btype='highpass', fs=2))
-
+        IIR = IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp1, ftype = 'high')
+        iir = signal.ellip(self.n, self.Rp, self.Rs, self.Wp1, btype='highpass', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+       　
     def test_ellip_4(self):
         # Test case for bandpass filter with default
-        self.assertTrue(IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp2) == signal.ellip(self.n, self.Rp, self.Rs, self.Wp2, btype='bandpass', fs=2))
-
+        IIR = IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp2)
+        iir = ignal.ellip(self.n, self.Rp, self.Rs, self.Wp2, btype='bandpass', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_ellip_5(self):
         # Test case for bandpass filter without default
-        self.assertTrue(IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp2, ftype = 'bandpass') == signal.ellip(self.n, self.Rp, self.Rs, self.Wp2, btype='bandpass', fs=2))
-
+        IIR = IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp2, ftype = 'bandpass')
+        iir = signal.ellip(self.n, self.Rp, self.Rs, self.Wp2, btype='bandpass', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_ellip_6(self):
         # Test case for bandstop filter
-        self.assertTrue(IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp2, ftype = 'stop') == signal.ellip(self.n, self.Rp, self.Rs, self.Wp2, btype='bandstop', fs=2))
-
+        IIR = IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wp2, ftype = 'stop')
+        iir = signal.ellip(self.n, self.Rp, self.Rs, self.Wp2, btype='bandstop', fs=2)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
     def test_ellip_7(self):
         # Test case for analog filter
-        self.assertTrue(IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wps, zs='s') == signal.ellip(self.n, self.Rp, self.Rs, self.Wps, btype='lowpass', analog=True))
+        IIR = IIRDesign.ellip(self.n, self.Rp, self.Rs, self.Wps, zs='s')
+        iir = signal.ellip(self.n, self.Rp, self.Rs, self.Wps, btype='lowpass', analog=True)
+        self.assertTrue((IIR[0] == iir[0]).all() and (IIR[1] == iir[1]).all())
+        
