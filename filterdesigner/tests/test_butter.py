e2691c8de5529b767c064a5add2bca69ccbd096f
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-07 12:59:34 +0900

    Update test_butter.py
    Add test case for Exception.

--- filterdesigner/tests/test_butter.py
+++ filterdesigner/tests/test_butter.py
@@ -53,4 +53,48 @@ class TestButter(unittest.TestCase):
         IIR = IIRDesign.butter(self.n, self.fcs, zs='s')
         iir = signal.butter(self.n, self.fcs, analog=True)
         self.assertTrue((IIR[0] == iir[0]).all and (IIR[1] == iir[1]).all)
-        
+
+    def test_butter_8(self):
+        # Test case for Exception 1
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(3.5, self.fc)
+
+    def test_butter_9(self):
+        # Test case for Exception 2
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n, self.fc, ftype='x')
+
+    def test_butter_10(self):
+        # Test case for Exception 3
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n, self.fcs, zs='x')
+
+    def test_butter_11(self):
+        # Test case for Exception 4
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n2, [0.75, 1.5])
+    
+    def test_butter_12(self):
+        # Test case for Exception 5
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n, 1.5)
+
+    def test_butter_13(self):
+        # Test case for Exception 6
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n2, self.fc2, ftype='low')
+    
+    def test_butter_14(self):
+        # Test case for Exception 7
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n2, self.fc2, ftype='high')
+
+    def test_butter_15(self):
+        # Test case for Exception 8
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n, self.fc, ftype='stop')
+
+    def test_batter_16(self):
+        # Test case for Exception 9
+        with self.assertRaises(ValueError):
+            IIRDesign.butter(self.n, self.fc, ftype='bandpass')
