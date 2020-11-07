bb08287fbc21a3d3848d7744331e345378010144
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-07 23:26:32 +0900

    Update test_iirpeak.py
    Add test case for exception.

--- filterdesigner/tests/test_iirpeak.py
+++ filterdesigner/tests/test_iirpeak.py
@@ -9,8 +9,18 @@ class TestIirpeak(unittest.TestCase):
         self.w0 = 0.4
         self.bw = 0.4/35
 
-    def test_iirpeak(self):
+    def test_iirpeak_1(self):
         # Test case
         IIR = IIRDesign.iirpeak(self.w0, self.bw)
         iir = signal.iirpeak(self.w0, self.w0/self.bw, fs=2)
         self.assertTrue(np.all(IIR[0] == iir[0]) and np.all(IIR[1] == iir[1]))
+
+    def test_iirpeak_2(self):
+        # Test case for Exception 1
+        with self.assertRaises(ValueError):
+            IIRDesign.iirpeak(4, self.bw)
+
+    def test_iirpeak_3(self):
+        # Test case for Exception 2
+        with self.assertRaises(ValueError):
+            IIRDesign.iirpeak(self.w0, 40)
