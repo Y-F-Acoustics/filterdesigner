926e27c3bb253364b7365bbe3e432242b5486ddb
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-07 23:17:03 +0900

    Update test_iirnotch.py
    Add test case for exception.

--- filterdesigner/tests/test_iirnotch.py
+++ filterdesigner/tests/test_iirnotch.py
@@ -9,8 +9,18 @@ class TestIirnotch(unittest.TestCase):
         self.w0 = 0.4
         self.bw = 0.4/35
 
-    def test_iirnotch(self):
+    def test_iirnotch_1(self):
         # Test case
         IIR = IIRDesign.iirnotch(self.w0, self.bw)
         iir = signal.iirnotch(self.w0, self.w0/self.bw, fs=2)
         self.assertTrue(np.all(IIR[0] == iir[0]) and np.all(IIR[1] == iir[1]))
+
+    def test_iirnotch_2(self):
+        # Test case for Exception 1
+        with self.assertRaises(ValueError):
+            IIRDesign.iirnotch(4, self.bw)
+
+    def test_iirnotch_3(self):
+        # Test case for Exception 2
+        with self.assertRaises(ValueError):
+            IIRDesign.iirnotch(self.w0, 40)
