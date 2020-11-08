06a9969ac3fec576b48d366208101d98272b7397
Yuki Fukuda <34057035+Y-F-Acoustics@users.noreply.github.com>
2020-11-08 16:43:18 +0900

    Update test_zplane.py
    Add test case for exception

--- filterdesigner/tests/test_zplane.py
+++ filterdesigner/tests/test_zplane.py
@@ -9,13 +9,23 @@ class TestZplane(unittest.TestCase):
         self.order = 80
         self.cut = 0.5
 
-    def test_zplane(self):
+    def test_zplane_1(self):
         # Test case
         fil = FIRDesign.fir1(self.order, self.cut)
         b = fil[0]
         a = fil[1]
         z, p, k = signal.tf2zpk(b, a)
         Z, P, K = FilterSpec.zplane(fil, show=False)
+
+        self.assertTrue(np.all(Z == z) and np.all(P == p) and (K == k))
+
+    def test_zplane_2(self):
+        # Test case with show
+        fil = FIRDesign.fir1(self.order, self.cut)
+        b = fil[0]
+        a = fil[1]
+        z, p, k = signal.tf2zpk(b, a)
+        Z, P, K = FilterSpec.zplane(fil, show=True)
 
         self.assertTrue(np.all(Z == z) and np.all(P == p) and (K == k))
         
